fde86ac2a898e24386ac18ffc82bef3948bf42ca
parsariyahi <parsanintendo12@gmail.com>
2022-01-16 14:52:21 +0330

    complerte db_oprations|make some dirs to organized main dir|create messege object

--- database/db_oprations.py
+++ database/db_oprations.py
@@ -1,5 +1,7 @@
+from hashlib import md5
 import mysql.connector
 
+#TODO move this in to main file and use it from there
 mydb = mysql.connector.connect(
   host="localhost",
   database="pars_messenger",
@@ -8,9 +10,10 @@ mydb = mysql.connector.connect(
 )
 
 #get all users data
-def user_get_data(db, username) :
+def user_get_data(db, username, password) :
     cur = db.cursor()
-    cur.execute(f'SELECT * FROM `users` WHERE `username` = "parsa" ')
+    password = md5(password.encode('utf8')).hexdigest()
+    cur.execute(f'SELECT * FROM `users` WHERE `username` = "{username}" AND `password` = "{password}" ')
     res = cur.fetchall()
     return res
 
@@ -18,13 +21,17 @@ def user_get_data(db, username) :
 def user_add(db, user_data:dict) :
     cur = db.cursor()
     username = user_data.get('username', None)
+    if not username :
+        return False
+        #TODO raise an error here
     full_name = user_data.get('full_name', None)
     age = user_data.get('age', None)
     email = user_data.get('email', None)
     phone_number = user_data.get('phone_number', None)
     bio = user_data.get('bio', None)
     privacy_status = user_data.get('privacy_status', None)
-    password = user_data.get('password', None)
+    password = user_data.get('password') 
+    password = md5(password.encode('utf8')).hexdigest()
 
     cur.execute(f'INSERT INTO `users` VALUES (0, "{username}", "{full_name}", "{age}", "{email}", "{phone_number}", "{bio}", "{privacy_status}", "{password}") ')
     db.commit()
@@ -35,6 +42,7 @@ def friend_request_send(db, from_, to) :
     cur.execute(f'INSERT INTO `friend_requests` VALUES (0, "{from_}", "{to}", 0)')
     db.commit()
 
+#craete a vertex and update the friend_request status to 1 -> means that is accepted
 def friend_request_accept(db, from_, to) :
     cur = db.cursor()
     cur.execute(f'INSERT INTO `vertices` VALUES (0, "{from_}", "{to}")')
@@ -44,30 +52,29 @@ def friend_request_accept(db, from_, to) :
     db.commit()
 
 
+#update the friend_request status to 2 -> means a request that is rejected
 def friend_request_reject(db, from_, to) :
     cur = db.cursor()
     cur.execute(f'UPDATE `friend_requests` SET `status` = 2 WHERE `from` = "{from_}" AND `to` = "{to}" ')
     db.commit()
 
-
-user = user_get_data(mydb, 'mina')
-
-print(user)
-
-
-friend_request_send(mydb, 'ali', 'mina')
-friend_request_accept(mydb, 'parsa', 'mina')
-friend_request_reject(mydb, 'ali', 'mina')
+#send a messege to someone
+def messege_send(db, from_, to, text) :
+    cur = db.cursor()
+    cur.execute(f'INSERT INTO `messeges` VALUES (0, "{from_}", "{to}", "{text}")')
+    db.commit()
 
 #data = {
-#        "username" : "ali",
-#        "full_name" : "ali por",
+#        "username" : "mmd",
+#        "full_name" : "mmd por",
 #        "age" : 20,
-#        "email" : "ali@gmail.com",
-#        "phone_number" : "09123456758",
+#        "email" : "mmd@gmail.com",
+#        "phone_number" : "09123456986",
 #        "bio" : "bad bitch",
-#        "privacy_status" : 0, 
-#        "password" : "ali1234",
+#        "privacy_status" : 1, 
+#        "password" : "mmd1234",
 #        }
 #
 #user_add(mydb, data)
+#user = user_get_data(mydb, "mmd", "mmd1234")
+#print(user)
---
+++ objects/Messege.py
@@ -0,0 +1,5 @@
+class Messege :
+    def __init__(self, from_, to, text) :
+        self.from_ = from_
+        self.to = to
+        self.text = text
